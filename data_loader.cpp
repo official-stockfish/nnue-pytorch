78d6fdfa4cb57f98482441b2dc3a30adf8521efb
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-11-07 15:16:29 +0100

    Simple unbatched training data passing.

--- data_loader.cpp
+++ data_loader.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <algorithm>
+#include <iterator>
 
 #include "nnue_data_binpack_format.h"
 #include "training_data_stream.h"
@@ -11,18 +15,155 @@
 #define CDECL __attribute__ ((__cdecl__))
 #endif
 
+using namespace binpack;
+using namespace chess;
+
+struct TestDataCollection
+{
+    int size;
+    int* data;
+};
+
+struct InputStreamHandle
+{
+    std::unique_ptr<training_data::BasicSfenInputStream> stream;
+};
+
+struct TrainingEntryHalfKPDense
+{
+    static constexpr int NUM_SQ = 64;
+    static constexpr int NUM_PT = 10;
+    static constexpr int NUM_PLANES = (NUM_SQ * NUM_PT + 1);
+    static constexpr int INPUTS = NUM_PLANES * NUM_SQ;
+
+    TrainingEntryHalfKPDense(const TrainingDataEntry& e)
+    {
+        us = static_cast<float>(e.pos.sideToMove());
+        outcome = (e.result + 1.0f) / 2.0f;
+        score = e.score;
+        std::fill(std::begin(white), std::end(white), 0.0f);
+        std::fill(std::begin(black), std::end(black), 0.0f);
+        fill_features(e);
+    }
+
+    float us;
+    float outcome;
+    float score;
+    float white[INPUTS];
+    float black[INPUTS];
+
+private:
+
+    static Square orient(Color color, Square sq)
+    {
+        if (color == Color::White)
+        {
+            return sq;
+        }
+        else
+        {
+            return sq.flippedVertically();
+        }
+    }
+
+    static int halfkp_idx(Color color, Square ksq, Square sq, Piece p)
+    {
+        auto p_idx = static_cast<int>(p.type()) * 2 + (p.color() != color);
+        return 1 + static_cast<int>(orient(color, sq)) + p_idx * NUM_SQ + static_cast<int>(ksq) * NUM_PLANES;
+    }
+
+    void fill_features(const TrainingDataEntry& e, float* features, Color color)
+    {
+        auto& pos = e.pos;
+        auto pieces = pos.piecesBB() & ~(pos.piecesBB(Piece(PieceType::King, Color::White)) | pos.piecesBB(Piece(PieceType::King, Color::Black)));
+        auto ksq = pos.kingSquare(color);
+        for(Square sq : pieces)
+        {
+            auto p = pos.pieceAt(sq);
+            features[halfkp_idx(color, orient(color, ksq), sq, p)] = 1.0f;
+        }
+    }
+
+    void fill_features(const TrainingDataEntry& e)
+    {
+        fill_features(e, white, Color::White);
+        fill_features(e, black, Color::Black);
+    }
+};
+
+struct TrainingEntryHalfKPSparse
+{
+    static constexpr int NUM_SQ = 64;
+    static constexpr int NUM_PT = 10;
+    static constexpr int NUM_PLANES = (NUM_SQ * NUM_PT + 1);
+    static constexpr int INPUTS = NUM_PLANES * NUM_SQ;
+    static constexpr int MAX_ACTIVE_FEATURES = 32;
+
+    TrainingEntryHalfKPSparse(const TrainingDataEntry& e)
+    {
+        us = static_cast<float>(e.pos.sideToMove());
+        outcome = (e.result + 1.0f) / 2.0f;
+        score = e.score;
+        num_active_white_features = 0;
+        num_active_black_features = 0;
+        std::fill(std::begin(white), std::end(white), 0.0f);
+        std::fill(std::begin(black), std::end(black), 0.0f);
+        fill_features(e);
+    }
+
+    float us;
+    float outcome;
+    float score;
+    int num_active_white_features;
+    int num_active_black_features;
+    int white[MAX_ACTIVE_FEATURES];
+    int black[MAX_ACTIVE_FEATURES];
+
+private:
+
+    static Square orient(Color color, Square sq)
+    {
+        if (color == Color::White)
+        {
+            return sq;
+        }
+        else
+        {
+            return sq.flippedVertically();
+        }
+    }
+
+    static int halfkp_idx(Color color, Square ksq, Square sq, Piece p)
+    {
+        auto p_idx = static_cast<int>(p.type()) * 2 + (p.color() != color);
+        return 1 + static_cast<int>(orient(color, sq)) + p_idx * NUM_SQ + static_cast<int>(ksq) * NUM_PLANES;
+    }
+
+    void fill_features(const TrainingDataEntry& e, int* features, int& counter, Color color)
+    {
+        auto& pos = e.pos;
+        auto pieces = pos.piecesBB() & ~(pos.piecesBB(Piece(PieceType::King, Color::White)) | pos.piecesBB(Piece(PieceType::King, Color::Black)));
+        auto ksq = pos.kingSquare(color);
+        for(Square sq : pieces)
+        {
+            auto p = pos.pieceAt(sq);
+            features[counter++] = halfkp_idx(color, orient(color, ksq), sq, p);
+        }
+    }
+
+    void fill_features(const TrainingDataEntry& e)
+    {
+        fill_features(e, white, num_active_white_features, Color::White);
+        fill_features(e, black, num_active_black_features,Color::Black);
+    }
+};
+
 extern "C" {
 
     EXPORT void CDECL test()
     {
         std::cout<< "test successful\n";
     }
-
-    struct TestDataCollection
-    {
-        int size;
-        int* data;
-    };
 
     EXPORT TestDataCollection* CDECL create_data_collection()
     {
@@ -33,6 +174,69 @@ extern "C" {
     {
         delete ptr->data;
         delete ptr;
+    }
+
+    EXPORT InputStreamHandle* CDECL create_stream(const char* filename)
+    {
+        auto stream = training_data::open_sfen_input_file(filename);
+        return new InputStreamHandle{std::move(stream)};
+    }
+
+    EXPORT void CDECL destroy_stream(InputStreamHandle* stream_handle)
+    {
+        delete stream_handle;
+    }
+
+    EXPORT void CDECL print_next(InputStreamHandle* stream_handle)
+    {
+        auto& stream = *(stream_handle->stream);
+        auto value = stream.next();
+        if (value.has_value())
+        {
+            std::cout << value->pos.fen() << '\n';
+        }
+        else
+        {
+            std::cout << "Nothing.\n";
+        }
+    }
+
+    EXPORT TrainingEntryHalfKPDense* CDECL get_next_entry_halfkp_dense(InputStreamHandle* stream_handle)
+    {
+        auto& stream = *(stream_handle->stream);
+        auto value = stream.next();
+        if (value.has_value())
+        {
+            return new TrainingEntryHalfKPDense(*value);
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    EXPORT TrainingEntryHalfKPSparse* CDECL get_next_entry_halfkp_sparse(InputStreamHandle* stream_handle)
+    {
+        auto& stream = *(stream_handle->stream);
+        auto value = stream.next();
+        if (value.has_value())
+        {
+            return new TrainingEntryHalfKPSparse(*value);
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    EXPORT void CDECL destroy_entry_halfkp_dense(TrainingEntryHalfKPDense* e)
+    {
+        delete e;
+    }
+
+    EXPORT void CDECL destroy_entry_halfkp_sparse(TrainingEntryHalfKPSparse* e)
+    {
+        delete e;
     }
 
 }
--- test_dll_call.py
+++ test_dll_call.py
@@ -41,6 +41,72 @@ print(tensor)
 
 destroy_data_collection(v)
 
+'''
 for i in range(1000000):
     v = create_data_collection()
     destroy_data_collection(v)
+'''
+
+class TrainingEntryHalfKPDense(ctypes.Structure):
+    _fields_ = [
+        ('us', ctypes.c_float),
+        ('outcome', ctypes.c_float),
+        ('score', ctypes.c_float),
+        ('white', ctypes.c_float * 41024),
+        ('black', ctypes.c_float * 41024)
+    ]
+
+    def get_tensors(self):
+        us = torch.tensor([self.us])
+        them = torch.tensor([1.0 - self.us])
+        outcome = torch.tensor([self.outcome])
+        score = torch.tensor([self.score])
+        white = torch.tensor(np.ctypeslib.as_array(self.white))
+        black = torch.tensor(np.ctypeslib.as_array(self.black))
+        return us, them, white, black, outcome, score
+
+class TrainingEntryHalfKPSparse(ctypes.Structure):
+    _fields_ = [
+        ('us', ctypes.c_float),
+        ('outcome', ctypes.c_float),
+        ('score', ctypes.c_float),
+        ('num_active_white_features', ctypes.c_int),
+        ('num_active_black_features', ctypes.c_int),
+        ('white', ctypes.c_int * 32),
+        ('black', ctypes.c_int * 32)
+    ]
+
+    def get_tensors(self):
+        us = torch.tensor([self.us])
+        them = torch.tensor([1.0 - self.us])
+        outcome = torch.tensor([self.outcome])
+        score = torch.tensor([self.score])
+        white = torch.empty(41024, layout=torch.sparse_coo)
+        black = torch.empty(41024, layout=torch.sparse_coo)
+        for i in range(self.num_active_white_features):
+            white[self.white[i]] = 1.0
+        for i in range(self.num_active_black_features):
+            black[self.black[i]] = 1.0
+        return us, them, white, black, outcome, score
+
+TrainingEntryHalfKPDensePtr = ctypes.POINTER(TrainingEntryHalfKPDense)
+TrainingEntryHalfKPSparsePtr = ctypes.POINTER(TrainingEntryHalfKPSparse)
+
+create_stream = dll.create_stream
+destroy_stream = dll.destroy_stream
+print_next = dll.print_next
+get_next_entry_halfkp_dense = dll.get_next_entry_halfkp_dense
+get_next_entry_halfkp_dense.restype = TrainingEntryHalfKPDensePtr
+destroy_entry_halfkp_dense = dll.destroy_entry_halfkp_dense
+get_next_entry_halfkp_sparse = dll.get_next_entry_halfkp_sparse
+get_next_entry_halfkp_sparse.restype = TrainingEntryHalfKPSparsePtr
+destroy_entry_halfkp_sparse = dll.destroy_entry_halfkp_sparse
+
+stream = create_stream(b'd10_10000.bin')
+print(stream)
+for i in range(100):
+    e = get_next_entry_halfkp_sparse(stream)
+    e.contents.get_tensors()
+    destroy_entry_halfkp_sparse(e)
+
+destroy_stream(stream)
