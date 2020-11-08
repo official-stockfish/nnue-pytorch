95203cede483925671d7aa1d43bb7efb7087be3e
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-11-08 11:08:28 +0100

    Compile with RelWithDebInfo by default.

--- compile_data_loader.bat
+++ compile_data_loader.bat
@@ -1,2 +1,2 @@
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="./"
-cmake --build ./build --config Release --target install
+cmake . -Bbuild -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="./"
+cmake --build ./build --config RelWithDebInfo --target install
