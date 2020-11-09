b25ed980826c2473bf94af595618ba718b2910b0
Gary Linscott <glinscott@gmail.com>
2020-11-08 16:30:37 -0800

    Add some comments

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from torch import nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 
+# 3 layer fully connected network
 L1 = 256
 L2 = 32
 L3 = 32
@@ -13,6 +14,11 @@ def cp_conversion(x, alpha=0.0016):
   return (x * alpha).sigmoid()
 
 class NNUE(pl.LightningModule):
+  """
+  This model attempts to directly represent the nodchip Stockfish trainer methodology.
+
+  It is not ideal for training a Pytorch quantized model directly.
+  """
   def __init__(self, feature_set=halfkp):
     super(NNUE, self).__init__()
     self.input = nn.Linear(feature_set.INPUTS, L1)
@@ -24,6 +30,7 @@ class NNUE(pl.LightningModule):
     w = self.input(w_in)
     b = self.input(b_in)
     l0_ = (us * torch.cat([w, b], dim=1)) + (them * torch.cat([b, w], dim=1))
+    # clamp here is used as a clipped relu to (0.0, 1.0)
     l0_ = torch.clamp(l0_, 0.0, 1.0)
     l1_ = torch.clamp(self.l1(l0_), 0.0, 1.0)
     l2_ = torch.clamp(self.l2(l1_), 0.0, 1.0)
@@ -33,7 +40,9 @@ class NNUE(pl.LightningModule):
   def step_(self, batch, batch_idx, loss_type):
     us, them, white, black, outcome, score = batch
     output = self(us, them, white, black)
-    loss = F.mse_loss(output, score)
+    # Scale by 600.0 here to avoid the final layer having to generate huge
+    # weights.  This constant must be adjusted when quantizing as well.
+    loss = F.mse_loss(output * 600.0, score)
     self.log(loss_type, loss)
     return loss
 
