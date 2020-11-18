2b2ff38a989947adcc02ae5739a0958a1f8c5596
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2020-11-18 22:13:21 +0100

    Add some additional arguments and output (#9)
    show filename of training and validation data
    
    allow to set the number of torch threads
    allow to set the batch size

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import nnue_dataset
 import nnue_bin_dataset
 import pytorch_lightning as pl
 import halfkp
+from torch import set_num_threads as t_set_num_threads
 from pytorch_lightning import loggers as pl_loggers
 from torch.utils.data import DataLoader, Dataset
 
@@ -20,11 +21,10 @@ class FixedNumBatchesDataset(Dataset):
   def __getitem__(self, idx):
     return next(self.iter)
 
-def data_loader_cc(train_filename, val_filename, num_workers):
+def data_loader_cc(train_filename, val_filename, num_workers, batch_size):
   # Epoch and validation sizes are arbitrary
   epoch_size = 100000000
   val_size = 1000000
-  batch_size = 8192
   train_infinite = nnue_dataset.SparseBatchDataset(halfkp.NAME, train_filename, batch_size, num_workers=num_workers)
   val_infinite = nnue_dataset.SparseBatchDataset(halfkp.NAME, val_filename, batch_size)
   # num_workers has to be 0 for sparse, and 1 for dense
@@ -33,8 +33,8 @@ def data_loader_cc(train_filename, val_filename, num_workers):
   val = DataLoader(FixedNumBatchesDataset(val_infinite, (val_size + batch_size - 1) // batch_size), batch_size=None, batch_sampler=None)
   return train, val
 
-def data_loader_py(train_filename, val_filename):
-  train = DataLoader(nnue_bin_dataset.NNUEBinData(train_filename), batch_size=128, shuffle=True, num_workers=4)
+def data_loader_py(train_filename, val_filename, batch_size):
+  train = DataLoader(nnue_bin_dataset.NNUEBinData(train_filename), batch_size=batch_size, shuffle=True, num_workers=4)
   val = DataLoader(nnue_bin_dataset.NNUEBinData(val_filename), batch_size=32)
   return train, val
 
@@ -46,16 +46,29 @@ def main():
   parser.add_argument("--py-data", action="store_true", help="Use python data loader (default=False)")
   parser.add_argument("--lambda", default=1.0, type=float, dest='lambda_', help="lambda=1.0 = train on evaluations, lambda=0.0 = train on game results, interpolates between (default=1.0).")
   parser.add_argument("--num-workers", default=1, type=int, dest='num_workers', help="Number of worker threads to use for data loading. Currently only works well for binpack.")
+  parser.add_argument("--batch-size", default=-1, type=int, dest='batch_size', help="Number of positions per batch / per iteration. Default on GPU = 8192 on CPU = 128.")
+  parser.add_argument("--threads", default=-1, type=int, dest='threads', help="Number of torch threads to use. Default automatic (cores) .")
   args = parser.parse_args()
 
   nnue = M.NNUE(halfkp, lambda_=args.lambda_)
 
+  print("Training with {} validating with {}".format(args.train, args.val))
+
+  batch_size = args.batch_size
+  if batch_size <= 0:
+    batch_size = 128 if args.gpus == 0 else 8192
+  print('Using batch size {}'.format(batch_size))
+
+  if args.threads > 0:
+    print('limiting torch to {} threads.'.format(args.threads))
+    t_set_num_threads(args.threads)
+
   if args.py_data:
     print('Using python data loader')
-    train, val = data_loader_py(args.train, args.val)
+    train, val = data_loader_py(args.train, args.val, batch_size)
   else:
     print('Using c++ data loader')
-    train, val = data_loader_cc(args.train, args.val, args.num_workers)
+    train, val = data_loader_cc(args.train, args.val, args.num_workers, batch_size)
 
   tb_logger = pl_loggers.TensorBoardLogger('logs/')
   trainer = pl.Trainer.from_argparse_args(args, logger=tb_logger)
