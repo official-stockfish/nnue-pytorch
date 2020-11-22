25e28d956964dc80c9447f03989eee595705de5c
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-11-22 10:41:56 +0100

    Keep each model on their device forever to avoid allocations, use copy_ which should be faster than deepcopy.

--- model.py
+++ model.py
@@ -52,18 +52,18 @@ class NNUE(pl.LightningModule):
     self.lambda_ = lambda_
     self.feature_set = feature_set
 
-    self.model = nn.Sequential(
+    self.models = [nn.Sequential(
       FeatureTransformer(feature_set),
       nn.Linear(2 * L1, L2),
       nn.Hardtanh(min_val=0.0, max_val=1.0),
       nn.Linear(L2, L3),
       nn.Hardtanh(min_val=0.0, max_val=1.0),
-      nn.Linear(L3, 1)).to(self.main_device)
+      nn.Linear(L3, 1)).to(device) for device in self.devices]
 
-    self.optimizer = ranger.Ranger(self.parameters())
+    self.optimizer = ranger.Ranger(self.models[0].parameters())
 
   def forward(self, us, them, w_in, b_in):
-    return self.model((us, them, w_in, b_in))
+    return self.models[0]((us, them, w_in, b_in))
 
   def backward(self, loss, optimizer, optimizer_idx):
     # We override backward to do nothing because we want to do
@@ -76,16 +76,18 @@ class NNUE(pl.LightningModule):
     if len(uss) != len(self.devices):
       raise Exception('Number of batches doesn\'t match the number of devices')
 
-    local_models = [self.model] + [copy.deepcopy(self.model).to(device=device) for device in self.devices[1:]]
+    with torch.no_grad():
+      for model in self.models[1:]:
+        for param, main_param in zip(model.parameters(), self.models[0].parameters()):
+          param.copy_(main_param, non_blocking=True)
 
-    qs = [m((u, t, w, b)) for m, u, t, w, b in zip(local_models, uss, thems, whites, blacks)]
+    qs = [m((u, t, w, b)) for m, u, t, w, b in zip(self.models, uss, thems, whites, blacks)]
     ts = outcomes
     # Divide score by 600.0 to match the expected NNUE scaling factor
     ps = [(score / 600.0).sigmoid() for score in scores]
 
     return {
-      'losses' : [self.loss_fn(q=q, t=t, p=p) / len(self.devices) for q,t,p in zip(qs, ts, ps)],
-      'local_models' : local_models
+      'losses' : [self.loss_fn(q=q, t=t, p=p) / len(self.devices) for q,t,p in zip(qs, ts, ps)]
       }
 
     # MSE Loss function for debugging
@@ -95,20 +97,19 @@ class NNUE(pl.LightningModule):
 
   def step_end_(self, batch_parts, loss_type, do_optimization=False):
     losses = batch_parts['losses']
-    local_models = batch_parts['local_models']
 
     if do_optimization:
-      for m in local_models:
+      for m in self.models:
           for param in m.parameters():
               param.grad = None
 
       for loss in losses:
         loss.backward()
 
-      grads_by_model = [[param.grad.to(device=self.main_device, non_blocking=True) for param in m.parameters()] for m in local_models[1:]]
+      grads_by_model = [[param.grad.to(device=self.main_device, non_blocking=True) for param in m.parameters()] for m in self.models[1:]]
 
       for grads in grads_by_model:
-          for main_param, grad in zip(self.model.parameters(), grads):
+          for main_param, grad in zip(self.models[0].parameters(), grads):
               main_param.grad += grad
 
       self.optimizer.step()
