08438133f48960f1c0166a4b18fe97c480b2a1cd
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-12-04 15:22:21 +0100

    Detect main device

--- train.py
+++ train.py
@@ -9,19 +9,19 @@ from torch import set_num_threads as t_set_num_threads
 from pytorch_lightning import loggers as pl_loggers
 from torch.utils.data import DataLoader, Dataset
 
-def data_loader_cc(train_filename, val_filename, features_name, num_workers, batch_size, filtered):
+def data_loader_cc(train_filename, val_filename, features_name, num_workers, batch_size, filtered, main_device):
   # Epoch and validation sizes are arbitrary
   epoch_size = 100000000
   val_size = 1000000
-  train_infinite = nnue_dataset.SparseBatchDataset(features_name, train_filename, batch_size, num_workers=num_workers, filtered=filtered)
-  val_infinite = nnue_dataset.SparseBatchDataset(features_name, val_filename, batch_size, filtered=filtered)
+  train_infinite = nnue_dataset.SparseBatchDataset(features_name, train_filename, batch_size, num_workers=num_workers, filtered=filtered, device=main_device)
+  val_infinite = nnue_dataset.SparseBatchDataset(features_name, val_filename, batch_size, filtered=filtered, device=main_device)
   # num_workers has to be 0 for sparse, and 1 for dense
   # it currently cannot work in parallel mode but it shouldn't need to
   train = DataLoader(nnue_dataset.FixedNumBatchesDataset(train_infinite, (epoch_size + batch_size - 1) // batch_size), batch_size=None, batch_sampler=None)
   val = DataLoader(nnue_dataset.FixedNumBatchesDataset(val_infinite, (val_size + batch_size - 1) // batch_size), batch_size=None, batch_sampler=None)
   return train, val
 
-def data_loader_py(train_filename, val_filename, batch_size):
+def data_loader_py(train_filename, val_filename, batch_size, main_device):
   train = DataLoader(nnue_bin_dataset.NNUEBinData(train_filename), batch_size=batch_size, shuffle=True, num_workers=4)
   val = DataLoader(nnue_bin_dataset.NNUEBinData(val_filename), batch_size=32)
   return train, val
@@ -74,19 +74,22 @@ def main():
     print('limiting torch to {} threads.'.format(args.threads))
     t_set_num_threads(args.threads)
 
-  if args.py_data:
-    print('Using python data loader')
-    train, val = data_loader_py(args.train, args.val, batch_size)
-  else:
-    print('Using c++ data loader')
-    train, val = data_loader_cc(args.train, args.val, features_name, args.num_workers, batch_size, args.smart_fen_skipping)
-
   logdir = args.default_root_dir if args.default_root_dir else 'logs/'
   print('Using log dir {}'.format(logdir), flush=True)
 
   tb_logger = pl_loggers.TensorBoardLogger(logdir)
   checkpoint_callback = pl.callbacks.ModelCheckpoint(save_last=True)
   trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback], logger=tb_logger)
+
+  main_device = trainer.root_device if trainer.root_gpu is None else 'cuda:' + str(trainer.root_gpu)
+
+  if args.py_data:
+    print('Using python data loader')
+    train, val = data_loader_py(args.train, args.val, batch_size, main_device)
+  else:
+    print('Using c++ data loader')
+    train, val = data_loader_cc(args.train, args.val, features_name, args.num_workers, batch_size, args.smart_fen_skipping, main_device)
+
   trainer.fit(nnue, train, val)
 
 if __name__ == '__main__':
