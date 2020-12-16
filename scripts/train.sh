1daadd81d018eff8b588fd745ac07419b1eaaada
Gary Linscott <glinscott@gmail.com>
2020-12-16 22:16:28 +0000

    Training on noob d12 data

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py ../data/d1_1b.binpack ../data/d10_128000_6293.binpack --gpus 1 --val_check_interval 2000 --threads 2 --batch-size 16384 --progress_bar_refresh_rate 20 --smart-fen-skipping --random-fen-skipping 10
+python train.py ../data4/d12_4b.binpack ../data3/noob-d20.binpack --gpus 1 --val_check_interval 2000 --threads 2 --batch-size 16384 --progress_bar_refresh_rate 20 --smart-fen-skipping --random-fen-skipping 7
