d342c45e749f8ba10bc3f2998500c460eb95efbe
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-08-09 22:07:23 +0200

    1024x2-8-32-1

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ import copy
 from feature_transformer import DoubleFeatureTransformerSlice
 
 # 3 layer fully connected network
-L1 = 512
-L2 = 16
+L1 = 1024
+L2 = 8
 L3 = 32
 
 def coalesce_ft_weights(model, layer):
