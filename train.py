01d53663c67602b4b702627e3d150cbdb0492810
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2022-05-16 09:50:53 +0200

    Update trainer to match new SFNNv5 network.

--- train.py
+++ train.py
@@ -30,6 +30,8 @@ def main():
   parser.add_argument("val", help="Validation data (.bin or .binpack)")
   parser = pl.Trainer.add_argparse_args(parser)
   parser.add_argument("--lambda", default=1.0, type=float, dest='lambda_', help="lambda=1.0 = train on evaluations, lambda=0.0 = train on game results, interpolates between (default=1.0).")
+  parser.add_argument("--start-lambda", default=None, type=float, dest='start_lambda', help="lambda to use at first epoch.")
+  parser.add_argument("--end-lambda", default=None, type=float, dest='end_lambda', help="lambda to use at last epoch.")
   parser.add_argument("--gamma", default=0.992, type=float, dest='gamma', help="Multiplicative factor applied to the learning rate after every epoch.")
   parser.add_argument("--lr", default=8.75e-4, type=float, dest='lr', help="Initial learning rate.")
   parser.add_argument("--num-workers", default=1, type=int, dest='num_workers', help="Number of worker threads to use for data loading. Currently only works well for binpack.")
@@ -49,14 +51,29 @@ def main():
   if not os.path.exists(args.val):
     raise Exception('{0} does not exist'.format(args.val))
 
+  if (args.start_lambda is not None) != (args.end_lambda is not None):
+    raise Exception('Either both or none of start_lambda and end_lambda must be specified.')
+
   feature_set = features.get_feature_set_from_name(args.features)
 
+  start_lambda = args.start_lambda or args.lambda_
+  end_lambda = args.end_lambda or args.lambda_
+  max_epoch = args.max_epochs or 800
   if args.resume_from_model is None:
-    nnue = M.NNUE(feature_set=feature_set, lambda_=args.lambda_, gamma=args.gamma, lr=args.lr)
+    nnue = M.NNUE(
+      feature_set=feature_set,
+      start_lambda=start_lambda,
+      max_epoch=max_epoch,
+      end_lambda=end_lambda,
+      gamma=args.gamma,
+      lr=args.lr
+    )
   else:
     nnue = torch.load(args.resume_from_model)
     nnue.set_feature_set(feature_set)
-    nnue.lambda_ = args.lambda_
+    nnue.start_lambda = start_lambda
+    nnue.end_lambda = end_lambda
+    nnue.max_epoch = max_epoch
     # we can set the following here just like that because when resuming
     # from .pt the optimizer is only created after the training is started
     nnue.gamma = args.gamma
