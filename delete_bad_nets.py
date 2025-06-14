670592669a503df17816950fb9909f89ab6b7f51
Disservin <disservin.social@gmail.com>
2025-06-14 21:12:13 +0200

    Ruff Format

--- delete_bad_nets.py
+++ delete_bad_nets.py
@@ -3,13 +3,14 @@ import re
 import os
 import itertools
 
+
 def parse_ordo(ordo_filename):
     ordo_scores = []
 
-    with open(ordo_filename, 'r') as ordo_file:
+    with open(ordo_filename, "r") as ordo_file:
         lines = ordo_file.readlines()
         for line in lines:
-            if 'nn-epoch' in line:
+            if "nn-epoch" in line:
                 fields = line.split()
                 net = fields[1]
                 rating = float(fields[3])
@@ -18,8 +19,9 @@ def parse_ordo(ordo_filename):
 
     return ordo_scores
 
+
 def find_ckpt_files(root_dir):
-    p = re.compile('.*\\.ckpt')
+    p = re.compile(".*\\.ckpt")
     ckpt_files = []
     for path, subdirs, files in os.walk(root_dir, followlinks=False):
         for filename in files:
@@ -28,8 +30,9 @@ def find_ckpt_files(root_dir):
                 ckpt_files.append(os.path.join(path, filename))
     return ckpt_files
 
+
 def find_nnue_files(root_dir):
-    p = re.compile('.*\\.nnue')
+    p = re.compile(".*\\.nnue")
     nnue_files = []
     for path, subdirs, files in os.walk(root_dir, followlinks=False):
         for filename in files:
@@ -38,14 +41,17 @@ def find_nnue_files(root_dir):
                 nnue_files.append(os.path.join(path, filename))
     return nnue_files
 
+
 def get_net_dir(net_path):
     return os.path.dirname(net_path)
 
+
 def split_nets_by_strength(nets, split_point=16):
     nets.sort(key=lambda x: -x[1])
-    best_nets = nets[:min(split_point, len(nets))]
-    worst_nets = nets[min(split_point, len(nets)):]
+    best_nets = nets[: min(split_point, len(nets))]
+    worst_nets = nets[min(split_point, len(nets)) :]
     return best_nets, worst_nets
+
 
 def get_nets_by_directory(best_nets, worst_nets, num_best_to_keep=16):
     binned_best_nets = dict()
@@ -68,75 +74,83 @@ def get_nets_by_directory(best_nets, worst_nets, num_best_to_keep=16):
 
     return binned_best_nets, binned_worst_nets
 
+
 def delete_bad_nets(root_dir, num_best_to_keep=16):
     net_epoch_p = re.compile(".*epoch([0-9]*)\\.nnue")
     ckpt_epoch_p = re.compile(".*epoch=([0-9]*).*\\.ckpt")
     ordo_filename = os.path.join(root_dir, "ordo.out")
     if not os.path.exists(ordo_filename):
-        print('No ordo file found. Exiting.')
+        print("No ordo file found. Exiting.")
         return
     else:
         nets = parse_ordo(ordo_filename)
         best_nets, worst_nets = split_nets_by_strength(nets, num_best_to_keep)
 
-        best_nets_by_dir, worst_nets_by_dir = get_nets_by_directory(best_nets, worst_nets, num_best_to_keep)
+        best_nets_by_dir, worst_nets_by_dir = get_nets_by_directory(
+            best_nets, worst_nets, num_best_to_keep
+        )
         for basedir, worst_nets_in_dir in worst_nets_by_dir.items():
             ckpt_files = find_ckpt_files(basedir)
             nnue_files = find_nnue_files(basedir)
-            worst_epochs = [net_epoch_p.match(net_name)[1] for net_name in worst_nets_in_dir]
+            worst_epochs = [
+                net_epoch_p.match(net_name)[1] for net_name in worst_nets_in_dir
+            ]
 
             for ckpt_file in ckpt_files:
                 try:
                     ckpt_epoch = ckpt_epoch_p.match(ckpt_file)[1]
                     if ckpt_epoch in worst_epochs:
-                        print('Delete {}'.format(ckpt_file))
+                        print("Delete {}".format(ckpt_file))
                         os.remove(ckpt_file)
                 except:
                     pass
 
-                print('Keep {}'.format(ckpt_file))
+                print("Keep {}".format(ckpt_file))
 
             for nnue_file in nnue_files:
                 try:
                     nnue_epoch = net_epoch_p.match(nnue_file)[1]
                     if nnue_epoch in worst_epochs:
-                        print('Delete {}'.format(nnue_file))
+                        print("Delete {}".format(nnue_file))
                         os.remove(nnue_file)
                 except:
                     pass
 
-                print('Keep {}'.format(nnue_file))
+                print("Keep {}".format(nnue_file))
 
 
 def show_help():
-    print('Usage: python delete_bad_nets.py root_dir [num_best_to_keep]')
+    print("Usage: python delete_bad_nets.py root_dir [num_best_to_keep]")
     print('root_dir - the directory to "cleanup"')
-    print('num_best_to_keep - the number of best nets to keep. Default: 16')
-    print('')
-    print('It expects to find ordo.out somewhere within root_dir.')
-    print('If the ordo.out is not found nothing is deleted.')
-    print('It uses the ratings from the ordo file to determine which nets are best.')
-    print('The engine names must contain the network name in the')
+    print("num_best_to_keep - the number of best nets to keep. Default: 16")
+    print("")
+    print("It expects to find ordo.out somewhere within root_dir.")
+    print("If the ordo.out is not found nothing is deleted.")
+    print("It uses the ratings from the ordo file to determine which nets are best.")
+    print("The engine names must contain the network name in the")
     print('following format: "nn-epoch[0-9]*\\.nnue". The network file')
-    print('can be specified with a parent directory (for example')
+    print("can be specified with a parent directory (for example")
     print('"run_0/nn-epoch100.nnue"), in which case the .ckpt file corresponding')
-    print('to this .nnue file will only be searched for in the parent ("run_0") directory.')
+    print(
+        'to this .nnue file will only be searched for in the parent ("run_0") directory.'
+    )
     print('The .ckpt files must contain "epoch=([0-9]*).*\\.ckpt".')
-    print('Both ckpt and nnue files are deleted. Only nets listed in the ordo')
-    print('file can be deleted. Other nets are always kept.')
-    print('The .nnue and .ckpt files are matched by epoch.')
-    print('')
-    print('The directory layout can be for example:')
-    print('- root_dir')
-    print('  - run_0')
-    print('    - a/b/c/d.ckpt')
-    print('    - *.nnue')
-    print('  - run_1')
-    print('    - a/b/c/d.ckpt')
-    print('    - *.nnue')
-    print('  - ordo.out')
-    print('    (in this case ony lines with engine name matching')
+    print("Both ckpt and nnue files are deleted. Only nets listed in the ordo")
+    print("file can be deleted. Other nets are always kept.")
+    print("The .nnue and .ckpt files are matched by epoch.")
+    print("")
+    print("The directory layout can be for example:")
+    print("- root_dir")
+    print("  - run_0")
+    print("    - a/b/c/d.ckpt")
+    print("    - *.nnue")
+    print("  - run_1")
+    print("    - a/b/c/d.ckpt")
+    print("    - *.nnue")
+    print("  - ordo.out")
+    print("    (in this case ony lines with engine name matching")
     print('     "run_[01]/nn-epoch[0-9]*\\.nnue" will be used.)')
+
 
 def main():
     if len(sys.argv) < 2:
@@ -147,5 +161,6 @@ def main():
     num_best_to_keep = sys.argv[2] if len(sys.argv) >= 3 else 16
     delete_bad_nets(root_dir, num_best_to_keep)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- feature_set.py
+++ feature_set.py
@@ -3,46 +3,53 @@ from feature_block import *
 import torch
 import chess
 
+
 def _calculate_features_hash(features):
     if len(features) == 1:
         return features[0].hash
 
     tail_hash = calculate_features_hash(features[1:])
-    return features[0].hash ^ (tail_hash << 1) ^ (tail_hash >> 1) & 0xffffffff
+    return features[0].hash ^ (tail_hash << 1) ^ (tail_hash >> 1) & 0xFFFFFFFF
+
 
 class FeatureSet:
-    '''
+    """
     A feature set is nothing more than a list of named FeatureBlocks.
     It itself functions similarily to a feature block, but we don't want to be
     explicit about it as we don't want it to be used as a building block for other
     feature sets. You can think of this class as a composite, but not the full extent.
     It is basically a concatenation of feature blocks.
-    '''
+    """
 
     def __init__(self, features):
         for feature in features:
             if not isinstance(feature, FeatureBlock):
-                raise Exception('All features must subclass FeatureBlock')
+                raise Exception("All features must subclass FeatureBlock")
 
         self.features = features
         self.hash = _calculate_features_hash(features)
-        self.name = '+'.join(feature.name for feature in features)
+        self.name = "+".join(feature.name for feature in features)
         self.num_real_features = sum(feature.num_real_features for feature in features)
-        self.num_virtual_features = sum(feature.num_virtual_features for feature in features)
+        self.num_virtual_features = sum(
+            feature.num_virtual_features for feature in features
+        )
         self.num_features = sum(feature.num_features for feature in features)
 
-    '''
+    """
     This method returns the feature ranges for the virtual factors of the
     underlying feature blocks. This is useful to know during initialization,
     when we want to zero initialize the virtual feature weights, but give some other
     values to the real feature weights.
-    '''
+    """
+
     def get_virtual_feature_ranges(self):
         ranges = []
         offset = 0
         for feature in self.features:
             if feature.num_virtual_features:
-                ranges.append((offset + feature.num_real_features, offset + feature.num_features))
+                ranges.append(
+                    (offset + feature.num_real_features, offset + feature.num_features)
+                )
             offset += feature.num_features
 
         return ranges
@@ -56,12 +63,13 @@ class FeatureSet:
 
         return ranges
 
-    '''
+    """
     This method goes over all of the feature blocks and gathers the active features.
     Each block has its own index space assigned so the features from two different
     blocks will never have the same index here. Basically the thing you would expect
     to happen after concatenating many feature blocks.
-    '''
+    """
+
     def get_active_features(self, board):
         w = torch.zeros(0)
         b = torch.zeros(0)
@@ -77,11 +85,12 @@ class FeatureSet:
 
         return w, b
 
-    '''
+    """
     This method takes a feature idx and looks for the block that owns it.
     If it found the block it asks it to factorize the index, otherwise
     it throws and Exception. The idx must refer to a real feature.
-    '''
+    """
+
     def get_feature_factors(self, idx):
         offset = 0
         for feature in self.features:
@@ -89,9 +98,9 @@ class FeatureSet:
                 return [offset + i for i in feature.get_feature_factors(idx - offset)]
             offset += feature.num_features
 
-        raise Exception('No feature block to factorize {}'.format(idx))
+        raise Exception("No feature block to factorize {}".format(idx))
 
-    '''
+    """
     This method does what get_feature_factors does but for all
     valid features at the same time. It returns a list of length
     self.num_real_features with ith element being a list of factors
@@ -100,7 +109,8 @@ class FeatureSet:
     slightly faster when there's many feature blocks. It might be worth
     to add a similar method to the FeatureBlock itself - to make it faster
     for feature blocks with many factors.
-    '''
+    """
+
     def get_virtual_to_real_features_gather_indices(self):
         indices = []
         real_offset = 0
