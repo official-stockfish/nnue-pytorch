531435c839a52f9d0a55e6c6e79b35335297f9d2
Shawn Xu <xu107288696@gmail.com>
2025-09-16 08:17:28 -0700

    organize feature sets to a subdirectory

--- features.py
+++ features.py
@@ -10,10 +10,10 @@ added to the _feature_modules list. Each such module must define a
 function `get_feature_block_clss` at module scope that returns the list
 of feature block classes in that module.
 """
-import halfkp
-import halfka
-import halfka_v2
-import halfka_v2_hm
+import features.halfkp as halfkp
+import features.halfka as halfka
+import features.halfka_v2 as halfka_v2
+import features.halfka_v2_hm as halfka_v2_hm
 
 _feature_modules = [halfkp, halfka, halfka_v2, halfka_v2_hm]
 
