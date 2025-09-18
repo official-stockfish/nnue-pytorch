56075381654f8ade05fafbc993b5451b61f09f0a
Shawn Xu <xu107288696@gmail.com>
2025-09-18 16:57:14 -0700

    asdf

--- compile_data_loader.bat
+++ compile_data_loader.bat
@@ -1,2 +1,10 @@
-cmake . -Bbuild -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="./"
-cmake --build ./build --config RelWithDebInfo --target install
+cmake -S . -B build-pgo-generate -DCMAKE_BUILD_TYPE=PGO_Generate
+cmake --build ./build-pgo-generate --config PGO_Generate
+
+./build-pgo-generate/training_data_loader_benchmark /home/shawn/Documents/Development/Python/nnue-pytorch/test77-dec2021-16tb7p.no-db.min.binpack
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=PGO_Use \
+    -DPGO_PROFILE_DATA_DIR=build-pgo-generate/pgo_data \
+    -DCMAKE_INSTALL_PREFIX="./"
+cmake --build ./build --config PGO_Use --target install
