445fc900bf4ac9dd149f6d693e70250dab444840
Shawn Xu <xu107288696@gmail.com>
2025-09-19 16:16:18 -0700

    small.binpack for pgo

--- compile_data_loader.bat
+++ compile_data_loader.bat
@@ -1,10 +1,12 @@
 cmake -S . -B build-pgo-generate -DCMAKE_BUILD_TYPE=PGO_Generate
 cmake --build ./build-pgo-generate --config PGO_Generate
 
-./build-pgo-generate/training_data_loader_benchmark /home/shawn/Documents/Development/Python/nnue-pytorch/test77-dec2021-16tb7p.no-db.min.binpack
+./build-pgo-generate/training_data_loader_benchmark .pgo/small.binpack
 
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=PGO_Use \
     -DPGO_PROFILE_DATA_DIR=build-pgo-generate/pgo_data \
     -DCMAKE_INSTALL_PREFIX="./"
 cmake --build ./build --config PGO_Use --target install
+
+rm -rf build-pgo-generate
