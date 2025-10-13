c8ad581b9226f414479e45e8fa9abe72ef76bb0c
xu-shawn <50402888+xu-shawn@users.noreply.github.com>
2025-10-13 13:09:08 -0700

    use a fixed seed for prng (#368)

--- lib/rng.h
+++ lib/rng.h
@@ -4,9 +4,10 @@
 
 namespace rng
 {
-    inline auto& get_thread_local_rng()
+    template <typename RNG = std::mt19937_64>
+    auto& get_thread_local_rng(typename RNG::result_type seed = RNG::default_seed)
     {
-        static thread_local std::mt19937_64 s_rng(std::random_device{}());
+        static thread_local RNG s_rng(seed);
         return s_rng;
     }
 }
