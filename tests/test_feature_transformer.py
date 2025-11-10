73ed7c625e9e96a5fb33fe3c627c1561c1c543e5
Shawn Xu <xu107288696@gmail.com>
2025-11-09 19:26:13 -0800

    fix

--- tests/test_feature_transformer.py
+++ tests/test_feature_transformer.py
@@ -1,5 +1,10 @@
+import os
+import sys
 import time
+
 import torch
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from model.modules import DoubleFeatureTransformerSlice
 from model.modules.feature_transformer.functions import DoubleFeatureTransformerSliceFunction
