27549f073b4a1aa8632277a2688642b18a32e316
sscg13 <iamdumbybear1@gmail.com>
2025-11-26 08:03:04 -0800

    only clip threat weights if featureset includes threats

--- model/model.py
+++ model/model.py
@@ -117,12 +117,13 @@ class NNUEModel(nn.Module):
                     p.data.copy_(p_data_fp32)
     
     def clip_threat_weights(self):
-        p = self.input.weight[0:79856]
-        p_data_fp32 = p.data
-        min_weight = -128 / 255
-        max_weight = 127 / 255
-        p_data_fp32.clamp_(min_weight, max_weight)
-        p.data.copy_(p_data_fp32)
+        if self.feature_set.name[:12] == "Full_Threats":
+            p = self.input.weight[0:79856]
+            p_data_fp32 = p.data
+            min_weight = -128 / 255
+            max_weight = 127 / 255
+            p_data_fp32.clamp_(min_weight, max_weight)
+            p.data.copy_(p_data_fp32)
 
     def set_feature_set(self, new_feature_set: FeatureSet):
         """
