737af0d7c1c23ae603081e7e6465d4f1f73af69b
FauziAkram <fauzi.dabat@hotmail.com>
2025-12-15 20:19:41 +0400

    Update run_games.py

--- run_games.py
+++ run_games.py
@@ -279,7 +279,7 @@ class EngineResults:
 
 def run_approximate_ordo(root_dir):
     """run an approximate ordo-like calculation on an existing pgn file"""
-    """ it takes advantege of the fact that all matches are ran against master """
+    """ it takes advantage of the fact that all matches are ran against master """
     pgn_file_name = os.path.join(root_dir, "out.pgn")
     ordo_file_name = os.path.join(root_dir, "ordo.out")
     ordo_file_name_temp = os.path.join(root_dir, "ordo_temp.out")
