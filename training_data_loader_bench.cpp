074a1482fda2693df8f62b5f7cc46b84282ebee3
Disservin <disservin.social@gmail.com>
2026-02-26 19:56:11 +0100

    Fix Dataloader Shared Library PGO
    case 1.
    link against cmake compiled shared library with pgo
    ```
    ➜  nnue-pytorch git:(pgo) ✗ g++ -std=c++20 -g3 -O3 -DNDEBUG -DBENCH -march=native \
        training_data_loader_bench.cpp \
        -L. -ltraining_data_loader \
        -Wl,-rpath,'$ORIGIN' \
        -o bench
    
    ➜  nnue-pytorch git:(pgo) ✗ ./bench ./.pgo/small.binpack
    Iter:     6000   Time(s):     45.6   MPos/s:     2.15   It/s:    1e+02   MB/s:    4e+02   B/pos:    2e+02
    ```
    
    case 2.
    for comparison, compile and run the benchmark without PGO optimizations
    
    ```
    ➜  nnue-pytorch git:(pgo) ✗ g++ -std=c++20 -g3 -O3 -DNDEBUG -DBENCH -march=native \
        training_data_loader_bench.cpp \
        training_data_loader.cpp \
        -o bench
    
    ➜  nnue-pytorch git:(pgo) ✗ ./bench ./.pgo/small.binpack
    Iter:     6000   Time(s):     52.2   MPos/s:     1.88   It/s:    1e+02   MB/s:    4e+02   B/pos:    2e+02
    ```
    
    old master shared library with shared linking via case 1.
    ```
    Iter:     6000   Time(s):     50.5   MPos/s:     1.95   It/s:    1e+02   MB/s:    4e+02   B/pos:    2e+02
    ```
    
    fix ci
    
    fix
    
    remove verbose

---
+++ training_data_loader_bench.cpp
@@ -0,0 +1,102 @@
+/*
+standalone dataloader bench
+
+g++ -std=c++20 -g3 -O3 -DNDEBUG -DBENCH -march=native \
+    training_data_loader_bench.cpp \
+    training_data_loader.cpp \
+    -o bench
+
+./bench /path/to/binpack
+*/
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <thread>
+
+#include "training_data_loader_abi.h"
+
+long long get_rchar_self() {
+    std::ifstream io_file("/proc/self/io");
+    std::string   line;
+    while (std::getline(io_file, line))
+    {
+        if (line.rfind("rchar:", 0) == 0)
+        {
+            return std::stoll(line.substr(6));
+        }
+    }
+    return -1;  // Error or not found
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " file1 [file2 ...]\n";
+        return 1;
+    }
+    const char** files      = const_cast<const char**>(&argv[1]);
+    int          file_count = argc - 1;
+
+#ifdef PGO_BUILD
+    const int concurrency = 1;
+#else
+    const int concurrency = std::thread::hardware_concurrency();
+#endif
+    // some typical numbers, more skipping means more load
+    const int                  batch_size = 16384;
+    const bool                 cyclic     = true;
+    const DataloaderSkipConfig config     = {.filtered             = true,
+                                             .random_fen_skipping  = 3,
+                                             .wld_filtered         = true,
+                                             .early_fen_skipping   = 5,
+                                             .simple_eval_skipping = 0,
+                                             .param_index          = 0,
+                                             .pc_y1                = 1.0,
+                                             .pc_y2                = 2.0,
+                                             .pc_y3                = 1.0};
+    const DataloaderDDPConfig  ddp_config = {.rank = 0, .world_size = 1};
+    auto stream = create_sparse_batch_stream("Full_Threats", concurrency, file_count, files,
+                                             batch_size, cyclic, config, ddp_config);
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+#ifdef PGO_BUILD
+    constexpr int iteration_count = 30;
+#else
+    constexpr int iteration_count = 6000;
+#endif
+
+    for (int i = 1; i <= iteration_count; ++i)
+    {
+        if (auto* b = fetch_next_sparse_batch(stream))
+        {
+            destroy_sparse_batch(b);
+        }
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+        if (i % 1 == 0)
+        {
+            double    sec   = (t1 - t0).count() / 1e9;
+            long long bytes = get_rchar_self();
+
+            double mpos = i * batch_size / (sec * 1000 * 1000);
+            double its  = i / sec;
+            double mbps = bytes / (sec * 1024 * 1024);
+            double bpos = bytes / (i * batch_size);
+
+            std::cout << "\rIter: " << std::setw(8) << i                                //
+                      << "   Time(s): " << std::setw(8) << std::setprecision(3) << sec  //
+                      << "   MPos/s: " << std::setw(8) << std::setprecision(3) << mpos  //
+                      << "   It/s: " << std::setw(8) << std::setprecision(1) << its     //
+                      << "   MB/s: " << std::setw(8) << std::setprecision(1) << mbps    //
+                      << "   B/pos: " << std::setw(8) << std::setprecision(1) << bpos   //
+                      << std::flush;
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
