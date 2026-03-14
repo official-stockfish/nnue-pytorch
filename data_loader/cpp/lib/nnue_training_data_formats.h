bee9242d248addb4914e6ce0c59d1dfad4654ff3
Tony <>
2026-03-14 10:13:06 -0700

    using ringCapacity=1 as testing revealed that there is no measurable speed difference

--- data_loader/cpp/lib/nnue_training_data_formats.h
+++ data_loader/cpp/lib/nnue_training_data_formats.h
@@ -7871,7 +7871,7 @@ namespace binpack
         };
 
         // Constant Size Ring Buffer
-        static constexpr int ringCapacity = 16;
+        static constexpr int ringCapacity = 1;
         thread_safe_types::ThreadLocalRegistry<LocalBuffer> m_bufferRegistry;
         thread_safe_types::ThreadSafeRingBuffer<TrainingDataEntries, ringCapacity> m_ringBuffer;
 
