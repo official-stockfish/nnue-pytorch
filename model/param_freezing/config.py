08bed39dfe8b01d74e7745cb13109253451009d7
Tony <>
2026-03-18 18:36:22 +0000

    fix typo

--- model/param_freezing/config.py
+++ model/param_freezing/config.py
@@ -7,14 +7,14 @@ class FreezeMode(Enum):
     """Modes for parameter freezing during training."""
     FULL_TRAINING = "full-training"
     PSQT_ONLY = "psqt-only"
-    FROZEN_PSQT = "frozen-pqst"
-    FROZEN_PSQT_FT = "frozen-pqst-ft"
+    FROZEN_PSQT = "frozen-psqt"
+    FROZEN_PSQT_FT = "frozen-psqt-ft"
 
 @dataclass
 class ParamFreezerConfig:
     param_freeze_mode: EnumChoicesFromValues[FreezeMode] = FreezeMode.FULL_TRAINING
     """Specifies which parameters are frozen.
     - full training: No parameters frozen.
-    - pqst only: Train pqst seperately as linear model.
-    - frozen pqst: pqst layers are frozen, others trainable.
-    - frozen pqst+ft: pqst and feature-transformer layers are frozen."""
+    - psqt only: Train psqt seperately as linear model.
+    - frozen psqt: psqt layers are frozen, others trainable.
+    - frozen psqt+ft: psqt and feature-transformer layers are frozen."""
