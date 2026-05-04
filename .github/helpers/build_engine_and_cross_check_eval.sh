28f58317a76abd83846c90c741adea13cf1ac9f0
Tony <Tony@secret.com>
2026-05-04 23:07:48 +0200

    use less insane approach to build Stockfish

--- .github/helpers/build_engine_and_cross_check_eval.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd "$SCRIPT_DIR"
-
-python -u build_compatible_engine.py --engine-dest-path /data/tmp/stockfish --overwrite
-python -u ../../cross_check_eval.py --net /data/logs/training/runs/unittests_train_pipeline_cpu/lightning_logs/version_2/checkpoints/last.nnue --data .pgo/small.binpack --engine /data/tmp/stockfish --build_engine_from_sha='' --device='cpu'
