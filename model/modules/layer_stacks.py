fix broadcasting of skip connections

model/modules/layer_stacks.py
@@ -36,7 +36,7 @@ class LayerStacks(nn.Module):
     ):
         l1c_ = self.l1(x, ls_indices, fake_quantize_weights)
         l1x_ = l1c_
-        l1x_out = l1c_[-2] - l1c_[-1]
+        l1x_out = l1c_[:, -2].view(-1, 1) - l1c_[:, -1].view(-1, 1)
 
         l1_sqr = torch.pow(l1x_, 2.0)
         if fake_quantize_acts:
@@ -52,7 +52,7 @@ class LayerStacks(nn.Module):
         l2c_ = self.l2(l1x_, ls_indices, fake_quantize_weights)
         if fake_quantize_acts:
             l2c_ = self.quantization.fake_quantize_ls_act(l2c_)
-        l2x_out = l2c_[-2] - l2c_[-1]
+        l2x_out = l2c_[:, -2].view(-1, 1) - l2c_[:, -1].view(-1, 1)
         l2x_ = self.quantization.clip_ls_act(l2c_)
 
         l3c_ = self.output(l2x_, ls_indices, fake_quantize_weights)
@@ -64,6 +64,7 @@ class LayerStacks(nn.Module):
         l3x_ = l3c_ + 2 * l1x_out + 2 * l2x_out
         if fake_quantize_acts:
             l3x_ = self.quantization.fake_quantize_output(l3x_)
+        assert l3x_.shape[1] == 1, f"Expected output shape (batch_size, 1), got {l3x_.shape}"
         return l3x_
 
     @torch.no_grad()