2384f27e8988a9192b463f4ca95c3219cceedc67
Tony Wang <35334345+TonyCongqianWang@users.noreply.github.com>
2026-07-04 05:52:03 +0200

    Update to SFNNv15: Better Skip Architecture (#480)
    * initial commit. using more skip connections
    
    * update crosscheck ref sha
    
    * fix broadcasting of skip connections
    
    * update ref sha
    
    * fix skip quantization order
    
    * new architecture
    
    * update crosscheck sha
    
    * Update Stockfish reference in workflow file
    
    * Update Stockfish reference in workflow file
    
    * update
    
    * use grad clip
    
    * apply score remapping to TB scores
    
    * Update training_data_loader.cpp
    
    * Update
    
    * remove redundant remap code
    
    * address PR comments:
    update crosscheck ref and remove unnecessary comments
    
    * remove unnecessary nl
    
    ---------
    
    Co-authored-by: Tony <Tony@secret.com>
    Co-authored-by: tony <tony@tony.com>

--- model/modules/config.py
+++ model/modules/config.py
@@ -9,7 +9,7 @@ import tyro
 class LayerStacksConfig:
     L1: Annotated[int, tyro.conf.arg(name="l1")] = 1024
     """Size of first hidden layer."""
-    L2: Annotated[int, tyro.conf.arg(name="l2")] = 31
+    L2: Annotated[int, tyro.conf.arg(name="l2")] = 32
     """Size of second hidden layer."""
     L3: Annotated[int, tyro.conf.arg(name="l3")] = 32
     """Size of third hidden layer."""
